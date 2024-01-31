98918add0fc3230f2633426ba0339fcf85b9951b
P R A D E E P <119108838+pradeepkrishh@users.noreply.github.com>
2024-01-31 11:44:01 +0530

    Add files via upload

---
+++ DAY2/DAY3/Home.jsx
@@ -0,0 +1,18 @@
+import './App.css';
+import NavBar from './NavBar'
+import SideBar from './SideBar';
+// import Student from '../src/images/student.jpg'
+// import './UserHome.css'
+
+function Home() {
+  return (
+    <div className='pic'>
+    <div>
+    <NavBar />
+    <SideBar/>
+    </div>
+    </div>
+     );
+  }
+  
+  export default Home;
